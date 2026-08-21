0ceac1a05818401957a5ae57b57842868ca9061b
Syed Hussain Ahmed <h2002ahmed@gmail.com>
2026-08-21 22:27:20 +0500

    remvoing all the AI generated files - Want to build it myself

--- src/sql.h
+++
@@ -1,8 +0,0 @@
-#ifndef SQL_H
-#define SQL_H
-
-/* Parses and executes one SQL statement, printing results/errors to
- * stdout. Returns 0 on success, -1 on parse/execution error. */
-int sql_execute(const char *sql);
-
-#endif
