1250d06f86ff72f04a266d53fc102db2880a94f8
Syed Hussain Ahmed <h2002ahmed@gmail.com>
2026-09-04 01:22:36 +0500

    modularized tokenizer.c file - created a standalone function that can be called in the main.c file

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,88 +1,80 @@
 #include <stdio.h>
 #include <string.h>
+#include "tokenizer.h"
 
-int main()
+static int is_special_char(char c)
 {
-    char string[] = "SELECT * FROM users";
-    char token[10][10] = {0}; // stores one key word at a time
+    return c == '=' || c == '+' || c == '-' || c == '/' || c == '*' || c == '(' || c == ')' || c == ';' || c == ',';
+}
+
+int tokenizer(char tokens[][10], char user_input[], int user_input_length)
+{
     int token_count = 0;
-    int i = 0, j = 0, k = 0; // i is the token#, j is the index of string,  k is the column#
+    int token_index = 0; // Which token am I currently building?
+    int char_index = 0;  // Which character of that token am I writing
 
-    printf("Input string: %s\n", string);
-
-    printf("Length of the string: %zu\n", strlen(string));
-
-    for (j = 0; j < strlen(string); j++)
+    for (int j = 0; j < user_input_length; j++)
     {
-        if (string[j] == ' ' && k > 0)
+        // white space region. No text encountered
+        if (user_input[j] == ' ' && char_index == 0)
         {
-            // Note:: Since k=0; Space was encountered on previous iteration
-            // No text was being parsed i.e. we are between tokens.
-
-            i++;   // new word so next token
-            k = 0; // marks the 0th index of the new token
-            token_count++;
-        }
-        else if (string[j] == ' ')
-        {
+            // Note:: Since char_index=0; Space was encountered on previous iteration
+            // No text was being read i.e. we are between tokens.
             continue;
         }
-        else if ((
-                     string[j] == '=' || string[j] == '+' || string[j] == '-' || string[j] == '/' || string[j] == '*' || string[j] == '(' || string[j] == ')' || string[j] == ';' || string[j] == ',') &&
-                 k > 0)
+
+        // below is code active when text is present
+        if (user_input[j] == ' ' && char_index > 0)
         {
-            // we were parsing a word. Then encountered a special char.
-
-            // finalize the word first. Then the special char
-            i++;
-            k = 0;
+            // space encountered.
+            // char_index > 0 i.e. a token was being read
+            // white space marks the end of the token
+            token_index++;  // new word so next token
+            char_index = 0; // marks the 0th index of the new token
             token_count++;
-
-            // next token is the special char
-            token[i][k] = string[j];
-            token_count++;
-
-            // move to the next token
-            i++;
         }
-        else if ((
-                     string[j] == '=' || string[j] == '+' || string[j] == '-' || string[j] == '/' || string[j] == '*' || string[j] == '(' || string[j] == ')' || string[j] == ';' || string[j] == ',') &&
-                 k == 0)
+        else if ((is_special_char(user_input[j])) && char_index > 0)
+        {
+            // we were reading a word. Then encountered a special character
+
+            // finalize the word.
+            token_index++;
+            char_index = 0;
+            token_count++;
+
+            // the special character is a separate token
+            tokens[token_index][char_index] = user_input[j];
+            token_count++;
+
+            // move to the next tokens
+            token_index++;
+        }
+        else if ((is_special_char(user_input[j])) && char_index == 0)
         {
             // we encountered space in prev iteration.
-            // already i++; k reset k= 0;
-            token[i][k] = string[j];
+            // already token_index++; char_index reset char_index= 0;
+            tokens[token_index][char_index] = user_input[j];
 
-            // move to the next token
-            i++;
+            // move to the next tokens
+            token_index++;
             token_count++;
         }
         else
         {
-            token[i][k] = string[j];
-            k++;
+            // Normal text. No special characters
+            tokens[token_index][char_index] = user_input[j];
+            char_index++;
         }
     }
 
-    if (k > 0)
+    if (char_index > 0)
     {
-        // there is one token which was not counted in the loop
+        // there is one tokens which was not counted in the loop
         // this could be because there is only one word in the input
         // the last word of the input
-        // k was never reset for both cases
+        // char_index was never reset for both cases
         token_count++;
     }
 
-    printf("Total number of tokens: %d\n", token_count);
-    for (i = 0; i < token_count; i++)
-    {
-        printf("Token%d = ", i);
-        for (j = 0; j < 10; j++)
-        {
-            printf("%c", token[i][j]);
-        }
-        printf("\n");
-    }
-
-    return 0;
+    return token_count;
 }
