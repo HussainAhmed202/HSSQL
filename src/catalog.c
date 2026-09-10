3b824c0350f85b202487239811af588b68864240
Syed Hussain Ahmed <h2002ahmed@gmail.com>
2026-09-10 22:45:05 +0500

    stack smashing occured - guardrails added so the code doesnt overwrite the memory

--- src/catalog.c
+++ src/catalog.c
@@ -2,10 +2,15 @@
 #include <string.h>
 #include "parser.h"
 
-// #define MAX_TABLES 5
+#define MAX_TABLES 5
 
 int add_table_to_catalog(TableSchema catalog[], int *num_tables, TableSchema new_table)
 {
+    if (*num_tables == MAX_TABLES)
+    {
+        return -1; // memory full
+    }
+
     // returns the number of tables stored in the database catalog
     catalog[*num_tables] = new_table;
     return ++*num_tables;
@@ -13,7 +18,7 @@ int add_table_to_catalog(TableSchema catalog[], int *num_tables, TableSchema new
 
 TableSchema *find_table_by_name(TableSchema catalog[], int num_tables, char table_name[])
 {
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < num_tables; i++)
     {
         if (strcmp(catalog[i].table_name, table_name) == 0)
         {
@@ -26,14 +31,19 @@ TableSchema *find_table_by_name(TableSchema catalog[], int num_tables, char tabl
 int main()
 {
     int num_of_tables = 0;
-    TableSchema catalog[2] = {0};
+    TableSchema catalog[MAX_TABLES] = {0};
+
     // CREATE TABLE user(NUMBER id, VARCHAR2 name)
     TableSchema table1 = {
         "user",
         {{"id", "NUMBER"}, {"name", "VARCHAR2"}},
         2};
 
-    printf("%d\n", add_table_to_catalog(catalog, &num_of_tables, table1));
+    if (add_table_to_catalog(catalog, &num_of_tables, table1) == -1)
+    {
+        printf("Memory full - No more tables can be created");
+        return -1;
+    }
 
     // CREATE TABLE transactions(NUMBER id, DOUBLE amount)
     TableSchema table2 = {
@@ -41,9 +51,25 @@ int main()
         {{"id", "NUMBER"}, {"amount", "DOUBLE"}},
         2};
 
-    printf("%d\n", add_table_to_catalog(catalog, &num_of_tables, table2));
+    if (add_table_to_catalog(catalog, &num_of_tables, table2) == -1)
+    {
+        printf("Memory full - No more tables can be created");
+        return -1;
+    }
 
-    for (int i = 0; i < 2; i++)
+    // CREATE TABLE bank(NUMBER id, Number user_id, DOUBLE amount, VARCHAR2 user_name)
+    TableSchema table3 = {
+        "bank",
+        {{"id", "NUMBER"}, {"user_id", "NUMBER"}, {"amount", "DOUBLE"}, {"name", "VARCHAR2"}},
+        4};
+
+    if (add_table_to_catalog(catalog, &num_of_tables, table3) == -1)
+    {
+        printf("Memory full - No more tables can be created");
+        return -1;
+    }
+
+    for (int i = 0; i < num_of_tables; i++)
     {
         printf("Table name: %s\n", catalog[i].table_name);
         printf("Total number of columns %d\n", catalog[i].num_columns);
